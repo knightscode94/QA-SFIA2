ec986e3dace8d62b0a64c34cd35d6713354e6850
Tobi Jackson <jtsproduction@live.com>
2020-08-20 16:27:56 +0100

    fixed demo

--- 8ball.py
+++ 8ball.py
@@ -3,64 +3,71 @@ import random
 
 question = input("Ask the magic 8 ball a question: ")
 
-answers = random.randint(1, 20)
+def answer():
 
-if answers == 1:
-    print("As I see it, yes.")
+    answers = random.randint(1, 20)
 
-elif answers == 2:
-    print("Ask again later.")
+    if answers == 1:
+        return "As I see it, yes."
 
-elif answers == 3:
-    print("Better not tell you now.")
+    elif answers == 2:
+        return "Ask again later."
 
-elif answers == 4:
-    print("Cannot predict now.")
+    elif answers == 3:
+        return "Better not tell you now."
 
-elif answers == 5:
-    print("Concentrate and ask again")
+    elif answers == 4:
+        return "Cannot predict now."
 
-elif answers == 6:
-    print("Don’t count on it.")
+    elif answers == 5:
+        return "Concentrate and ask again"
 
-elif answers == 7:
-    print("It is certain.")
+    elif answers == 6:
+        return "Don’t count on it."
 
-elif answers == 8:
-    print("It is decidedly so.")
+    elif answers == 7:
+        return "It is certain."
 
-elif answers == 9:
-    print("Most likely.")
+    elif answers == 8:
+        return "It is decidedly so."
 
-elif answers == 10:
-    print("My reply is no.")
+    elif answers == 9:
+        return "Most likely."
 
-elif answers == 11:
-    print("My sources say no.")
+    elif answers == 10:
+        return "My reply is no."
 
-elif answers == 12:
-    print("Outlook not so good.")
+    elif answers == 11:
+        return "My sources say no."
 
-elif answers == 13:
-    print("Outlook good.")
+    elif answers == 12:
+        return "Outlook not so good."
 
-elif answers == 14:
-    print("Reply hazy, try again.")
+    elif answers == 13:
+        return "Outlook good."
 
-elif answers == 15:
-    print("Signs point to yes.")
+    elif answers == 14:
+        return "Reply hazy, try again."
 
-elif answers == 16:
-    print("Very doubtful.")
+    elif answers == 15:
+        return "Signs point to yes."
 
-elif answers == 17:
-    print("Without a doubt.")
+    elif answers == 16:
+        return "Very doubtful."
 
-elif answers == 18:
-    print("Yes.")
+    elif answers == 17:
+        return "Without a doubt."
 
-elif answers == 19:
-    print("Yes – definitely.")
+    elif answers == 18:
+        return "Yes."
 
-elif answers == 20:
-    print("You may rely on it.")
+    elif answers == 19:
+        return "Yes – definitely."
+
+    elif answers == 20:
+        return "You may rely on it."
+
+    else:
+        return "Magic is lost..."
+
+print(answer())
