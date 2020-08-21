f78c4b9fdde1bb8c5fab1766fa955530e3c2c3d2
Tobi Jackson <jtsproduction@live.com>
2020-08-21 13:04:13 +0100

    DB

--- service1/application/models.py
+++ service1/application/models.py
@@ -2,4 +2,5 @@ from application import db
 
 class Responses(db.Model):
     id = db.Column(db.Interger, Primary_key=True)
+    questions = db.Column(db.String(100))
     responses = db.Column(db.String(50))
