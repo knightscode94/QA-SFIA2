4adafa05c158df9aaae7a721993fa28889da65e9
Tobi Jackson <jtsproduction@live.com>
2020-08-22 10:59:02 +0100

    please just show something on the front end!

--- service1/application/routes.py
+++ service1/application/routes.py
@@ -5,8 +5,5 @@ import requests
 
 @app.route('/', methods = ['GET'])
 def home():
-    question = requests.get('http://service2:5001/ball/questions')
-    response = requests.get('http://service3:5002//ball/response')
-
-    return render_template('home.html', title='Home', question=question.txt, response=response.txt)
+    return render_template('home.html', title='Home')
 
