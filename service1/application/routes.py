893351c47660d356d6b090c8c362cdf59d70889d
Tobi Jackson <jtsproduction@live.com>
2020-08-22 10:47:53 +0100

    front end

--- service1/application/routes.py
+++ service1/application/routes.py
@@ -2,11 +2,11 @@ from flask import render_template, redirect, url_for
 from application import app
 import requests
 
-@app.route('/', methods=['GET'])
+
+@app.route('/', methods = ['GET'])
 def home():
-    @app.route('/ball/start', methods = ['GET'])
-	username = requests.get('http://service2:5001/ball/questions')
-	game = requests.get('http://service3:5002//ball/response')
+    question = requests.get('http://service2:5001/ball/questions')
+    response = requests.get('http://service3:5002//ball/response')
 
     return render_template('home.html', title='Home', question=question.txt, response=response.txt)
 
