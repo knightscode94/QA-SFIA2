85dbb3d45fcd4da5cc17d1987b9bbbcc0a2bee38
Tobi Jackson <jtsproduction@live.com>
2020-08-22 12:59:20 +0100

    questions

--- service1/application/routes.py
+++ service1/application/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, requests
 from application import app
 import requests
 
@@ -6,6 +6,6 @@ import requests
 @app.route('/', methods = ['GET', 'POST'])
 def home():
     questions = requests.get('http://service2:5001/ball/questions')
-	answers = requests.get('http://service3:5002/ball/answers')
-    return render_template('home.html', title='Home', questions=questions.text, answers=answers.text)
+	#answers = requests.get('http://service3:5002/ball/answers')
+    return render_template('home.html', title='Home', questions=questions.text)
 
