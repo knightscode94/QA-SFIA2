b75d16308a66b90715ded0de9e32d8d3eecaad68
Tobi Jackson <jtsproduction@live.com>
2020-08-24 11:20:01 +0100

    service4

--- service1/create.py
+++
@@ -1,5 +0,0 @@
-from application import db
-from application.models import all_data
-
-db.drop_all()
-db.create_all()
--- service4/application/routes.py
+++ service4/application/routes.py
@@ -6,4 +6,9 @@ import random
 @app.route('/probability',methods=['GET','POST'])
 def probability():
     
+    data_sent = request.get_json()
+    question = data_sent.keys()
+    answer = data_sent.values()
+
+    
     return Response(probability, mimetype='text/plain')
