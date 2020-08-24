8cb6a010bc601cad49382ecb3406af6015c31dde
Tobi Jackson <jtsproduction@live.com>
2020-08-24 12:20:16 +0100

    s4

--- service4/application/routes.py
+++ service4/application/routes.py
@@ -7,8 +7,8 @@ import random
 @app.route('/probability', methods=['GET', 'POST'])
 def probability():
 
-    question = request.get_json(question)
-    answer = request.get_json(answer)
+    question = request.question.get_json()
+    answer = request.answer.get_json()
 
     if answer == "Not a chance" & question == "Will I ever be good enough at QA?":
         probability = "60% True"
