abf72a3e82732995c743e932c0f6710f4f562736
Tobi Jackson <jtsproduction@live.com>
2020-08-26 16:43:20 +0100

    nevermind

--- service1/application/models.py
+++ service1/application/models.py
@@ -4,4 +4,4 @@ class all_data(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     question = db.Column(db.String(100), nullable = False)
     answer = db.Column(db.String(100), nullable = False)
-    probability = db.Column(db.String, nullable = False)
+    probability = db.Column(db.String(100), nullable = False)
