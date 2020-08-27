5831c65218a68652e20345dbc9921e8309c44e5b
Tobi Jackson <jtsproduction@live.com>
2020-08-27 15:33:31 +0100

    s4

--- service4/application/routes.py
+++ service4/application/routes.py
@@ -9,35 +9,33 @@ def probability():
 
     data = request.get_json()
 
-    question = data.keys()
-    answer = data.values()
-    for key in question:
-        for value in answer:
-            if answer[value] == "Not a chance" & question[key] == "Will I ever be good enough at QA?":
-                probability = "60% True"
-            elif answer[value] == "Not a chance" & question[key] == "Is Luke the most epic lecturer at QA?":
-                probability = "10% True"
-            elif answer[value] == "Not a chance" & question[key] == "Will I live a long life?":
-                probability = "90%"
-            elif answer[value] == "Not a chance" & question[key] == "Is this a load of rubbish?":
-                probability = "100% True"
-            elif answer[value] == "Sure why not" & question[key] == "Will I ever be good enough at QA?":
-                probability = "30% True"
-            elif answer[value] == "Sure why not" & question[key] == "Is Luke the most epic lecturer at QA?":
-                probability = "90% True"
-            elif answer[value] == "Sure why not" & question[key] == "Will I live a long life?":
-                probability = "40% True"
-            elif answer[value] == "Sure why not" & question[key] == "Is this a load of rubbish?":
-                probability = "10% True"
-            elif answer[value] == "Errrr..." & question[key] == "Will I ever be good enough at QA?":
-                probability = "60% True"
-            elif anwer[value] == "Errrr..." & question[key] == "Is Luke the most epic lecturer at QA?":
-                probability = "20% True"
-            elif answer[value] == "Errrr..." & question[key] == "Will I live a long life?":
-                probability = "70% True"
-            elif answer[value] == "Errrr..." & question[key] == "Is this a load of rubbish?":
-                probability = "10% True"
-            else:
-                probability = "You wish I had an answer"
+    question = data["question"]
+    answer = data["answer"]
+    if answer == "Not a chance" & question == "Will I ever be good enough at QA?":
+        probability = "60% True"
+    elif answer == "Not a chance" & question == "Is Luke the most epic lecturer at QA?":
+        probability = "10% True"
+    elif answer == "Not a chance" & question == "Will I live a long life?":
+        probability = "90%"
+    elif answer == "Not a chance" & question == "Is this a load of rubbish?":
+        probability = "100% True"
+    elif answer == "Sure why not" & question == "Will I ever be good enough at QA?":
+        probability = "30% True"
+    elif answer == "Sure why not" & question == "Is Luke the most epic lecturer at QA?":
+        probability = "90% True"
+    elif answer == "Sure why not" & question == "Will I live a long life?":
+        probability = "40% True"
+    elif answer == "Sure why not" & question == "Is this a load of rubbish?":
+        probability = "10% True"
+    elif answer == "Errrr..." & question == "Will I ever be good enough at QA?":
+        probability = "60% True"
+    elif answer == "Errrr..." & question == "Is Luke the most epic lecturer at QA?":
+        probability = "20% True"
+    elif answer == "Errrr..." & question == "Will I live a long life?":
+        probability = "70% True"
+    elif answer == "Errrr..." & question == "Is this a load of rubbish?":
+        probability = "10% True"
+    else:
+        probability = "You wish I had an answer"
 
     return Response(probability, mimetype='text/plain')
