82eccc04aa3eaba41eb8f7a250fc7a6a827edf3c
Tobi Jackson <jtsproduction@live.com>
2020-08-28 14:46:51 +0100

    tests

--- service1/tests/test_service1.py
+++ service1/tests/test_service1.py
@@ -6,7 +6,7 @@ import requests_mock
   
 from flask import url_for
 from flask_testing import TestCase
-from application import app,db
+from application import app, db
 from application.models import all_data
 from os import getenv
 
@@ -31,7 +31,6 @@ class TestBase(TestCase):
 
 
     def tearDown(self):
-
         db.session.remove()
         db.drop_all()
 
@@ -46,5 +45,5 @@ class TestService1(TestBase):
                     m.get('http://service2:5001/question', text='"Will I live a long life?')
                     m.get('http://service3:5002/answer', text='Sure why not')
                     m.post('http://service4:5003/probability', text='50% True')                
-                    resp = self.client.get(url_for('ball'))
-                    self.assertEqual(resp.status_code, 200)
+                    response = self.client.get(url_for('ball'))
+                    self.assertEqual(response.status_code, 200)
