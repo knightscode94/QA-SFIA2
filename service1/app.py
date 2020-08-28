b6b3a7805cfb1a3d317c11229d5157549cfa709b
Tobi Jackson <jtsproduction@live.com>
2020-08-28 16:12:57 +0100

    tests

--- service1/app.py
+++ service1/app.py
@@ -2,8 +2,5 @@ from application import app
 from application import db
 from application.models import all_data
 
-db.drop_all()
-db.create_all()
-
 if __name__ == "__main__":
     app.run(port=5000, debug=True, host='0.0.0.0')
